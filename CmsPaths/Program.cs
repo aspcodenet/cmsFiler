cb21ebf41396ece802d58f326b04f7dbf969fa94
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-09-26 13:14:40 +0200

    fixes

--- CmsPaths/Program.cs
+++ CmsPaths/Program.cs
@@ -10,7 +10,12 @@ namespace CmsPaths
     {
         static void Main(string[] args)
         {
-            using(var s = System.IO.File.OpenText("c:\\kurser\\hopp.txt"))
+            using(var f = System.IO.File.CreateText("..\\..\\newFile.txt"))
+            {
+                f.WriteLine("Hello");
+            }
+
+            using(var s = System.IO.File.OpenText("..\\..\\account.txt"))
             {
                 var alltext = s.ReadToEnd();
                 Console.WriteLine(alltext);
