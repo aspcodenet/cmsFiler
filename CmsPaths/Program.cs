f654324bfcaafbb9ed479f3c6ef5c575d43c4a43
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-09-26 13:07:44 +0200

    Add project files.

---
+++ CmsPaths/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmsPaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var s = System.IO.File.OpenText("c:\\kurser\\hopp.txt"))
+            {
+                var alltext = s.ReadToEnd();
+                Console.WriteLine(alltext);
+            }
+        }
+    }
+}
